214525916b40253220e2c02cf20b2314f72e803d
AMRIT PAL SINGH <126061621+amt-12@users.noreply.github.com>
2026-04-11 09:49:14 +0530

    mm

--- .history/server_20260411094252.js
+++ .history/server_20260411094252.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: "*",
+  origin: "https://abhishekjudicialacademy-backend.vercel.app",
   credentials: true
 }));
 
